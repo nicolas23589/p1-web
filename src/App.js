7aa656e0f2ff9ac7c38f004aefbb2569da498fbe
Nicolás23589 <n.murilloc@uniandes.edu.co>
2024-09-27 09:12:51 -0500

    terminando

--- src/App.js
+++ src/App.js
@@ -1,11 +1,85 @@
 import './App.css';
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, useNavigate } from 'react-router-dom';
+import bannerimg from './banner.jpg'; 
+import RobotList from './RobotList';
 
-function App() {
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState('');
+  const navigate = useNavigate();
+
+  const handleLogin = () => {
+    const validUsername = 'usuario';
+    const validPassword = 'contraseña';
+
+    if (username === validUsername && password === validPassword) {
+      navigate('/home');
+    } else {
+      setError('Error de autenticación. Revise sus credenciales');
+    }
+  };
+
   return (
-    <div className="App">
-      <p>Hola mundo 2</p>
+    <div style={{ textAlign: 'center', marginTop: '10px' }}>
+      <h1>Adopta un robot con RobotLovers!</h1>
+      <img 
+        src= {bannerimg }
+        alt="Banner" 
+        style={{ width: '75%', height: '10%' }}
+      />
+      <h2>Inicio de Sesión</h2>
+      <p> Elemento visto solo en modo desarrollo, el usuario y el password son: 'usuario' y 'contraseña'</p>
+      <div>
+        <p>Nombre de usuario:</p>
+        <input 
+          type="text" 
+          placeholder="" 
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+          style={{ display: 'block', margin: '10px auto' }}
+        />
+        <p  style={{textAlign: 'center'}} >Contraseña: </p>
+        <input 
+          type="password" 
+          placeholder="" 
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          style={{ display: 'block', margin: '10px auto' }}
+        />
+      </div>
+      {error && <p style={{ color: 'red' }}>{error}</p>}
+      <div>
+        <button 
+          onClick={handleLogin} 
+          style={{ backgroundColor: 'blue', color: 'white', marginRight: '10px' }}>
+          Ingresar
+        </button>
+        <button 
+          onClick={() => navigate('/')} 
+          style={{ backgroundColor: 'red', color: 'white' }}>
+          Cancelar
+        </button>
+      </div>
+      <footer style={{ marginTop: '20px' }}>
+        <p>Contact us: +57 3102105253 - info@robot-lovers.com - @robot-lovers</p>
+      </footer>
     </div>
   );
 }
 
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<RobotList />} />
+      </Routes>
+    </Router>
+  );
+}
+
 export default App;
+
---
+++ src/RobotList.js
@@ -0,0 +1,94 @@
+import bannerimg from './banner.jpg';
+import React, { useState } from 'react';
+
+function RobotList() {
+  const defaultImg = 'https://m.media-amazon.com/images/I/31MqR2pPE5L._AC_.jpg'; 
+  const products = [
+    { id: 1, nombre: 'Pedrito', modelo: 'NRG-500', empresa: 'Robotico Corp', year: 2020, capacidad: '2.5 GHz', humor: 'Alegre y juguetón, tiene un comportamiento similar al de un gatito curios, siempre metiendose en temas de tecnología', img:defaultImg},
+    { id: 2, nombre: 'Iron chef', modelo: 'NRG-5001', empresa: 'Robotico Corp', year: 2021, capacidad: '3.0 GHz', humor: 'Alegre y juguetón, tiene un comportamiento similar al de un gatito curios, siempre metiendose en temas de tecnología',img:defaultImg },
+    { id: 3, nombre: 'chispita', modelo: 'NRG-500 C', empresa: 'Robotico Corp Z', year: 2022, capacidad: '3.5 GHz', humor: 'Alegre y juguetón, tiene un comportamiento similar al de un gatito curios, siempre metiendose en temas de tecnología',img:defaultImg },
+    { id: 1, nombre: 'sir calculin', modelo: 'NRG-500 A', empresa: 'Robotico Corp X', year: 2020, capacidad: '2.5 GHz', humor: 'Alegre y juguetón, tiene un comportamiento similar al de un gatito curios, siempre metiendose en temas de tecnología',img:defaultImg},
+    { id: 2, nombre: 'doctora bot', modelo: 'NRG-500 B', empresa: 'Robotico Corp Y', year: 2021, capacidad: '3.0 GHz', humor: 'Alegre y juguetón, tiene un comportamiento similar al de un gatito curios, siempre metiendose en temas de tecnología',img:defaultImg },
+    { id: 3, nombre: 'Zumba bot', modelo: 'NRG-500 C', empresa: 'Robotico Corp Z', year: 2022, capacidad: '3.5 GHz', humor: 'Alegre y juguetón, tiene un comportamiento similar al de un gatito curios, siempre metiendose en temas de tecnología',img:defaultImg },
+  ];
+
+  const [selectedProduct, setSelectedProduct] = useState(null);
+
+  const handleProductClick = (product) => {
+    setSelectedProduct(product);
+  };
+
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Adopta un robot con RobotLovers!</h1>
+      <img 
+        src={bannerimg}
+        alt="Banner" 
+        style={{ width: '75%', height: '10%' }}
+      />
+
+   
+      <div className='row' style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+
+        <div className='col' style={{ width: '50%' }}>
+          <h2>Listado de Robots</h2>
+          <table style={{ margin: '0 auto', borderCollapse: 'collapse', width: '100%' }}>
+            <thead>
+              <tr style={{ backgroundColor: '#333', color: '#fff' }}>
+                <th style={{ padding: '10px', border: '1px solid #ddd' }}>ID</th>
+                <th style={{ padding: '10px', border: '1px solid #ddd' }}>Nombre</th>
+                <th style={{ padding: '10px', border: '1px solid #ddd' }}>Modelo</th>
+                <th style={{ padding: '10px', border: '1px solid #ddd' }}>Empresa Fabricante</th>
+              </tr>
+            </thead>
+            <tbody>
+              {products.map((product) => (
+                <tr 
+                  key={product.id} 
+                  style={{ backgroundColor: '#f2f2f2', cursor: 'pointer' }} 
+                  onClick={() => handleProductClick(product)}
+                >
+                  <td style={{ padding: '10px', border: '1px solid #ddd' }}>{product.id}</td>
+                  <td style={{ padding: '10px', border: '1px solid #ddd' }}>{product.nombre}</td>
+                  <td style={{ padding: '10px', border: '1px solid #ddd' }}>{product.modelo}</td>
+                  <td style={{ padding: '10px', border: '1px solid #ddd' }}>{product.empresa}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
+        <div className='col' style={{ width: '300px', marginLeft: '20px' }}>
+          {selectedProduct && (
+            <div 
+              style={{ 
+                backgroundColor: '#f2f2f2', 
+                border: '2px solid #333', 
+                padding: '20px', 
+                textAlign: 'left' 
+              }}
+            >
+              <h2 style={{textAlign:'center'}}>{selectedProduct.nombre}</h2>
+              <img  
+                src={selectedProduct.img }  
+                alt="Imagen de Producto" 
+                style={{ 
+                  width: '120px', 
+                  textAlign:'center',
+                  height: '120px', 
+                  border: '2px solid #333', 
+                  marginBottom: '20px' 
+                }} 
+              />
+              <p><strong> - Año de fabricación:</strong> {selectedProduct.year}</p>
+              <p><strong> - Capacidad de procesamiento:</strong> {selectedProduct.capacidad}</p>
+              <p><strong> - Humor:</strong> {selectedProduct.humor}</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default RobotList;
